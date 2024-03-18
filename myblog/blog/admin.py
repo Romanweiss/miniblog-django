a080317ea298673facad79aed06b99bc33babed4
Romanweib <romanweib@gmail.com>
2024-03-18 14:14:07 +0300

    model Post is created model Post' showing is fixed first post is created

--- myblog/blog/admin.py
+++ myblog/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "author"]
+    list_display_links = ["title", "author"]
